6bb6ed67ee1fcf98cade08f2c23cd0263348f88c
iskenderColak <iclk84@gmail.com>
2023-03-07 18:25:20 +0300

    Return Statements

---
+++ src/com/icolak/day13_customMethods/Test.java
@@ -0,0 +1,16 @@
+package com.icolak.day13_customMethods;
+
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+      //  double n2 = input.close();
+
+      //  String word = System.out.println("Hello");
+    }
+}
