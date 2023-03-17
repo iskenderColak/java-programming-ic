bef6fac183be5ed51b665c340639ee044abdd076
iskenderColak <iclk84@gmail.com>
2023-03-17 11:37:05 +0300

    inheritance

---
+++ src/com/icolak/day29_inheritance/animalTask/Cat.java
@@ -0,0 +1,12 @@
+package com.icolak.day29_inheritance.animalTask;
+
+public class Cat extends Animal { // Cat Is An animal
+
+    public void meow() {
+        System.out.println(getName() + " is meowing");
+    }
+
+    public void scratch() {
+        System.out.println(getName() + " is scratching");
+    }
+}
---
+++ src/com/icolak/day29_inheritance/animalTask/Tiger.java
@@ -0,0 +1,7 @@
+package com.icolak.day29_inheritance.animalTask;
+
+public class Tiger extends Animal { // Tiger Is An Animal
+    public void hunt() {
+        System.out.println(getName() + " is hunting");
+    }
+}
