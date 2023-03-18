f5adddbbbaea73a247fc6d21d3b01af1a1860309
iskenderColak <iclk84@gmail.com>
2023-03-18 18:48:17 +0300

    Interface & Abstraction

---
+++ src/com/icolak/day35_polymorphism/transpotationTask/AutoPark.java
@@ -0,0 +1,6 @@
+package com.icolak.day35_polymorphism.transpotationTask;
+
+public interface AutoPark {
+    boolean hasAutoPark = true;
+    void autoPark();
+}
---
+++ src/com/icolak/day35_polymorphism/transpotationTask/Electric.java
@@ -0,0 +1,5 @@
+package com.icolak.day35_polymorphism.transpotationTask;
+
+public interface Electric {
+    void charge();
+}
