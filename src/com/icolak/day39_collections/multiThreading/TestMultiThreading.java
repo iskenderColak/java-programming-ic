e9fa9174d493734516815b780d41d9bc24409fc4
iskenderColak <iclk84@gmail.com>
2023-03-19 22:58:56 +0300

    start() method

--- src/com/icolak/day39_collections/multiThreading/TestMultiThreading.java
+++ src/com/icolak/day39_collections/multiThreading/TestMultiThreading.java
@@ -10,12 +10,16 @@ public class TestMultiThreading {
         ThreadHelloWorld thread4 = new ThreadHelloWorld(4);
         ThreadHelloWorld thread5 = new ThreadHelloWorld(5);
 
+/*
         thread1.run();
         thread2.run();
         thread3.run();
         thread4.run();
         thread5.run();
-
+*/
+        // if we want the threads get to be executed at the same time
+        // we have to use start method instead of run method. When we use start method,
+        // run method gets executed at the same time
         thread1.start();
         thread2.start();
         thread3.start();
